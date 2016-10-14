0271dd0ad4a7093b120466c3a5d47be8c8cb667b
suzuki kyohei <k-suzuki@adglobe.co.jp>
2016-10-14 18:45:11 +0900

    画面編集

--- src/application/views/top_view.php
+++ src/application/views/top_view.php
@@ -10,8 +10,11 @@
 </style>
 </head>
 <body bgcolor="#FF9933">
-  <from method="post" action="login/view">
-    <a href="music_view">
+  <header>
+<h1>趣味掲示板へようこそ！！</h1>
+</header>
+  <form method="post" action="home/music">
+    <a href="<?php echo base_url().'home/music'; ?>">
       <div style="position:absolute;top:300px;left:300px;">
         <img src="<?php echo base_url().'img/music.jpg'; ?>" width="370" height="220" >
       </a>
@@ -27,23 +30,27 @@
         <img src="<?php echo base_url().'img/game.jpg'; ?>" width="370" height="220" >
       </a>
     </div>
-  <a href="logout">
+  <a href="<?php echo base_url().'logout/'.$user_id; ?>">
     <div name="logout"style="position:absolute;top:100px;left:1600px;">
       <img src="<?php echo base_url().'img/logout.jpg'; ?>" width="250" height="60" >
   </a>
   <div style="position:absolute;top:500px;right:50px;"><ul id="normal" class="dropmenu">
-         <li><a href="#">menu</a>
+         <li>
+           <a href="#">カテゴリ</a>
              <ul>
-                 <li><a href="#">submenu</a></li>
-                 <li><a href="#">submenu</a></li>
-                 <li><a href="#">submenu</a></li>
-                 <li><a href="#">submenu</a></li>
+                 <li><a href="#">邦楽</a></li>
+                 <li><a href="#">洋楽</a></li>
+                 <li><a href="#">ロック</a></li>
+                 <li><a href="#">FPS</a></li>
+                 <li><a href="#">パズル</a></li>
+                 <li><a href="#">レース</a></li>
+                 <li><a href="#">サッカー</a></li>
+                 <li><a href="#">野球</a></li>
             </ul>
          </li>
 
-
-
 </div>
+
 </div>
 </body>
 </html>
