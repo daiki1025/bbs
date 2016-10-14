b492bca855bd3951a6a1ae412c296ed36f765074
suzuki kyohei <k-suzuki@adglobe.co.jp>
2016-10-14 18:44:26 +0900

    リダイレクト機能の追加

--- src/application/config/routes.php
+++ src/application/config/routes.php
@@ -52,3 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['home/(:any)'] = 'home/main/$1'; 
+$route['logout/(:any)'] = 'logout/out/$1';
