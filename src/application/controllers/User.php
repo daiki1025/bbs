0cb71df8950bbb32f9464e7e2efa334c52c8663a
suzuki kyohei <k-suzuki@adglobe.co.jp>
2016-10-14 18:44:09 +0900

    ユーザー登録機能の追加

---
+++ src/application/controllers/User.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+    public function __construct()
+    {
+  parent::__construct();
+  $this->load->database();
+}
+
+  public function index()
+  {
+   if($this->input->post('name') != null)
+    {
+      $data=array('id'=>$this->input->post('id',true),
+        'name'=>$this->input->post('name',true),
+      'pass'=>$this->input->post('pass',true));
+      $this->db->insert('user',$data);
+      $this->load->view('login_view');
+      redirect(base_url().'login');
+    }
+    else
+    {
+        $this->load->view('account_view');
+     }
+    }
+  }
--- src/application/views/account_view.php
+++ src/application/views/account_view.php
@@ -5,6 +5,12 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body bgcolor="#FF9933">
+  <font size="5" color="#FFFFFF">
+    <div style="position:absolute;top:70px;left:850px;">
+      <h2>アカウント名
+      </h2>
+    </div>
+  </font>
 <font size="5" color="#FFFFFF">
   <div style="position:absolute;top:220px;left:850px;">
     <h2>ユーザー名
@@ -16,21 +22,24 @@
       <h2>パスワード</h2>
     </div>
   </font>
-  <form action="login/touroku" method="post">
-    <div style="position:absolute;top:300px;left:750px;">
-    <input type="text"name="id1"style="width:400px;height:50px;">
+  <form action="<?php echo base_url().'User'; ?>" method="post">
+    <div style="position:absolute;top:150px;left:750px;">
+    <input type="text"name="id"style="width:400px;height:50px;">
   </div>
+  <div style="position:absolute;top:300px;left:750px;">
+  <input type="text"name="name"style="width:400px;height:50px;">
+</div>
     <div style="position:absolute;top:450px;left:750px;">
-    <input type="text"name="pass1"style="width:400px;height:50px;">
+    <input type="password"name="pass"style="width:400px;height:50px;">
   </div>
   <div style="position:absolute;top:600px;left:800px;">
     <button type="submit">
-      <img src="img/touroku.jpg" width="300" height="60" alt="">
+      <img src="<?php echo base_url().'img/touroku.jpg'; ?>" width="300" height="60" alt="">
     </button>
   </div>
     <a href="login">
       <div style="position:absolute;top:700px;left:1300px;">
-        <img src="img/back.jpg" width="250" height="60" alt="">
+        <img src="<?php echo base_url().'img/back.jpg'; ?>" width="250" height="60" alt="">
       </a>
     </form>
 </div>
